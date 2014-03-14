34d5663b93d4e8f344ba766e4188cc5a6b4da1fc
TheAcademician <TheAcademician@gmail.com>
2014-03-14 11:26:11 -0500

    Implemented horse damage and death prevention listeners.

--- src/us/drome/cobracorral/CorralListener.java
+++ src/us/drome/cobracorral/CorralListener.java
@@ -1,12 +1,15 @@
 package us.drome.cobracorral;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityTameEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class CorralListener implements Listener {
     private final CobraCorral plugin;
@@ -16,9 +19,30 @@ public class CorralListener implements Listener {
     }
     
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
-        Entity entity = event.getRightClicked();
-        if(entity instanceof Horse) {
+        Player player = event.getPlayer();
+        if(event.getRightClicked() instanceof Horse) {
+            Horse horse = (Horse)event.getRightClicked();
             
+            if(plugin.config.HORSES.containsKey(horse.getUniqueId())) {
+                if(!player.getName().equalsIgnoreCase(horse.getOwner().getName())) {
+                    
+                }
+            }
+            //check ownership, if now owner and locked, cancel.
+        } else {
+            if(player.hasMetadata(CobraCorral.HORSE_INFO)) {
+                player.sendMessage(ChatColor.GRAY + "You can only perform that action on a horse.");
+                player.removeMetadata(CobraCorral.HORSE_INFO, plugin);
+            } else if (player.hasMetadata(CobraCorral.HORSE_LOCK)) {
+                player.sendMessage(ChatColor.GRAY + "You can only perform that action on a horse.");
+                player.removeMetadata(CobraCorral.HORSE_LOCK, plugin);
+            } else if (player.hasMetadata(CobraCorral.HORSE_TEST_DRIVE)) {
+                player.sendMessage(ChatColor.GRAY + "You can only perform that action on a horse.");
+                player.removeMetadata(CobraCorral.HORSE_TEST_DRIVE, plugin);
+            } else if (player.hasMetadata(CobraCorral.HORSE_UNLOCK)) {
+                player.sendMessage(ChatColor.GRAY + "You can only perform that action on a horse.");
+                player.removeMetadata(CobraCorral.HORSE_UNLOCK, plugin);
+            }
         }
     }
     
@@ -31,6 +55,39 @@ public class CorralListener implements Listener {
             } else {
                 plugin.lockHorse(entity.getUniqueId(), owner.getName());
                 ((Player)owner).sendMessage("This horse has been locked.");
+            }
+        }
+    }
+    
+    public void onEntityDamage(EntityDamageEvent event) {
+        if(event.getEntity() instanceof Horse) {
+            Horse horse = (Horse)event.getEntity();
+            if(plugin.config.HORSES.containsKey(horse.getUniqueId())) {
+                if(horse.getPassenger() == null) {
+                    event.setCancelled(true);                    
+                }
+            }
+        }
+    }
+    
+    public void onEntityDeath(EntityDeathEvent event) {
+        if(event.getEntity() instanceof Horse) {
+            Horse horse = (Horse)event.getEntity();
+            if(plugin.config.HORSES.containsKey(horse.getUniqueId())) {
+                Player owner = (Player)horse.getOwner();
+                Player causedBy = (Player)horse.getPassenger();
+                if(!owner.getName().equalsIgnoreCase(causedBy.getName())) {
+                    if(owner.isOnline()) {
+                        owner.sendMessage(ChatColor.GRAY + horse.getCustomName() != null ? horse.getCustomName() : horse.getVariant().toString() +
+                            " has died due to the actions of " + causedBy.getName() + ".");
+                    }
+                } else {
+                    if(owner.isOnline()) {
+                        owner.sendMessage(ChatColor.GRAY + horse.getCustomName() != null ? horse.getCustomName() : horse.getVariant().toString() +
+                            " has died due to your actions.");
+                    }
+                }
+                plugin.config.HORSES.remove(horse.getUniqueId());
             }
         }
     }
