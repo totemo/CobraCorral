5ee6cd23af7b89ec8b6d65ef166c9f05fcc79b20
TheAcademician <TheAcademician@gmail.com>
2014-03-24 22:34:35 -0500

    Addressed issue with config loading/saving.

--- src/us/drome/cobracorral/Configuration.java
+++ src/us/drome/cobracorral/Configuration.java
@@ -17,6 +17,7 @@ public class Configuration {
     }
     
     public void save() {
+        reload();
         plugin.getConfig().set("horses", HORSES);
         plugin.saveConfig();
     }
@@ -37,12 +38,9 @@ public class Configuration {
     
     public void reload() {
         Map<String, LockedHorse> tempHorses = HORSES;
-        plugin.reloadConfig();
-        MAX_HORSES = plugin.getConfig().getInt("max-horses", 2);
-        IMMORTALITY = plugin.getConfig().getBoolean("immortality", true);
-        AUTO_LOCK = plugin.getConfig().getBoolean("auto-lock", true);
-        IMMORTAL_COOLDOWN = plugin.getConfig().getBoolean("immortal-cooldown", false);
-        COOLDOWN_TIME = plugin.getConfig().getInt("cooldown-time", 0);
+
+        load();
+        
         HORSES = tempHorses;
     }
 }
